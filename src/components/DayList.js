7c859bfc1637e77b3a39c2b03f88207141e6fc18
Oluwatobi Bello <bellotobi10@gmail.com>
2020-08-03 19:34:19 -0400

    Building/ the DayList in storybook

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  //console.log(props);
+  const { days } = props;
+
+  const lists = days.map((lists, index) => {
+    return (
+      <DayListItem
+        key={index}
+        name={lists.name}
+        spots={lists.spots}
+        selected={lists.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+  return <ul>{lists}</ul>;
+}
