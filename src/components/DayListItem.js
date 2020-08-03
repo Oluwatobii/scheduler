2cb7a90c9a08e04f96edd97cb06b74c69cf899c0
Oluwatobi Bello <bellotobi10@gmail.com>
2020-08-03 18:37:33 -0400

    Building/Styling/Testing Button & Building/Styling/Testing DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames/bind";
+
+export default function DayListItem(props) {
+  //console.log(props);
+  const dayClass = classNames("day-list", {
+    "day-list__item": props.name,
+    "day-list__item--selected": props.selected === true,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  function formatSpots(spots) {
+    if (spots < 1) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${spots} spots remaining`;
+    }
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="day-list">{props.name}</h2>
+      <h3 className="day-list">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
