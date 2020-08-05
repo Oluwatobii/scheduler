fb58d178da7c113169eba98405bddf6b01601621
Oluwatobi Bello <bellotobi10@gmail.com>
2020-08-04 21:48:35 -0400

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+
 import "components/Appointment/styles.scss";
-//import classNames from "classnames/bind";
+
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewers={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
