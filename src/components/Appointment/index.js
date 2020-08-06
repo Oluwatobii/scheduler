1a21d9626380791621923f52e956ef1c71dbe8f2
Oluwatobi Bello <bellotobi10@gmail.com>
2020-08-06 19:08:19 -0400

    Created and using custom hook for transitioning between states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,24 +1,44 @@
 import React from "react";
+import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+// import Status from "components/Appointment/Status";
+// import Confirm from "components/Appointment/Confirm";
+// import Error from "components/Appointment/Error";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  //console.log("THIS IS THE MODE BEING PASSED", mode);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
 
-      {props.interview ? (
+      {mode === EMPTY && (
+        <Empty
+          onAdd={(e) => {
+            transition(CREATE);
+          }}
+        />
+      )}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewers={props.interview.interviewer.name}
+          interviewer={props.interview.interviewer.name}
         />
-      ) : (
-        <Empty />
       )}
+      {mode === CREATE && <Form onCancel={back} interviewers={[]} />}
     </article>
   );
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  // Creating a function that will take in a new mode and update the mode state with the new value.
+  function transition(newMode, replace = false) {
+    if (replace) {
+      const newHistory = [...history];
+      newHistory[newHistory.length - 1] = newMode;
+      setHistory(newHistory);
+    } else {
+      setHistory((history) => [...history, newMode]);
+    }
+    setMode(newMode);
+  }
+
+  //A function to go back to the previous mode if the history is not in it's initial state
+  function back() {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+      setMode(newHistory[newHistory.length - 1]);
+      setHistory(newHistory);
+    }
+  }
+
+  return { mode, transition, back };
+}
