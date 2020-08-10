4a0b7358e6835b1f026c25bb7da9044549142419
Oluwatobi Bello <bellotobi10@gmail.com>
2020-08-10 15:14:57 -0400

    Writing test for the Appointment Component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
 
 import "components/InterviewerList.scss";
 
@@ -24,3 +25,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+};
